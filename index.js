b3685adbd8424f616cb8fc94297ccf57b543810e
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2024-04-25 14:46:48 -0500

    maganer - crear slice

--- index.js
+++ index.js
@@ -114,11 +114,14 @@ async function menu_manager(user){
     do {
         show_home_manager(user.username);
         answers = await inquirer.prompt(options_manager);
+        let selectedOptionName = options_manager[0].choices.find(choice => choice.value === answers.options_admin).name;
+
         switch(answers.options_admin){
             case 1: // show list of slices
-                await show_slices_list(user);
+                await show_slices_list_manag(user,selectedOptionName);
                 break;
             case 2: // create slice
+                await show_create_manag(user,selectedOptionName);
                 break;
             case 3: // manage slices
                 break;
@@ -141,20 +144,23 @@ async function menu_manager(user){
 
 async function show_home_manager(username){
     // fetch slices data from orchestrator server
-    console.log('-----\nHome > ' + username + '\n-----');
+    console.log('-----\n' + username + ' > Home \n-----');
 }
 
 async function show_slice_details(){
     const answer = await inquirer.prompt(show_slice_prompt);
     console.log(answer);
 }
+async function show_home_level1(username, optionName){
+    console.log('-----\n' + username + ' > Home > '+ optionName + '\n-----');
 
-async function show_slices_list(user){
-    console.log('-----\nHome > ' + user.username + ' > Lista de slices' + '\n-----');
+}
+async function show_slices_list_manag(user, optionName){
+    // 
+    show_home_level1(user.username,optionName)
     const answers = await inquirer.prompt(show_slices);
     switch(answers.show_slices){
         case 1:
-            console.log("Bienvenido al slice 1")
             break;
         case 2:
             break;
@@ -164,6 +170,89 @@ async function show_slices_list(user){
             menu_manager(user)
             break;
     }
+}
+
+
+async function show_create_manag(user, optionName){
+    show_home_level1(user.username, optionName);
+    const answers = await inquirer.prompt(show_create_options_mng);
+    switch(answers.create_options){
+        case 1:
+            console.log('Utilizando template...');
+            break;
+        case 2:
+            const sliceConfig = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'sliceName',
+                    message: 'Ingrese el nombre del slice:'
+                },
+                {
+                    type: 'list',
+                    name: 'topology',
+                    message: 'Seleccione una topología predefinida:',
+                    choices: ['Star', 'Tree', 'Mesh', 'Anillo']
+                },
+                {
+                    type: 'number',
+                    name: 'numVM',
+                    message: 'Número de VMs:',
+                    filter: Number
+                }
+            ]);
+            
+            const vmConfigs = [];
+            for (let i = 0; i < sliceConfig.numVM; i++) {
+                console.log(`Configuración de la VM #${i + 1}`);
+                const vmConfig = await inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'vmCPU',
+                        message: 'Ingrese la configuración de CPU: '
+                    },
+                    {
+                        type: 'input',
+                        name: 'vmMemory',
+                        message: 'Ingrese la configuración de memoria RAM: '
+                    },
+                    {
+                        type: 'input',
+                        name: 'vmStorage',
+                        message: 'Ingrese la configuración de almacenamiento: '
+                    },
+                    {
+                        type: 'list',
+                        name: 'vmAvailabilityZone',
+                        message: 'Seleccione la zona de disponibilidad: ',
+                        choices: ['Zona A', 'Zona B', 'Zona C']
+                    }
+                ]);
+                vmConfigs.push(vmConfig);
+            }
+
+            const sliceNetwork = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'securityRules',
+                    message: 'Seleccione una regla de acceso/salida a internet o cree una nueva:',
+                    choices: [
+                        'Permitir todo el tráfico saliente',
+                        'Bloquear todo el tráfico saliente',
+                        'Permitir todo el tráfico entrante',
+                        'Bloquear todo el tráfico entrante',
+                        'Permitir tráfico HTTP (puerto 80)',
+                        'Permitir tráfico HTTPS (puerto 443)',
+                        'Crear una nueva regla'
+                    ]
+                }
+            ]);
+            const spinner = createSpinner('Creando Slice ...').start();
+            setTimeout(() => {spinner.stop();}, 12000);
+            break;
+        case 3:
+            break;
+    }
+    menu_manager(user);
 }
 
 // ---------- CLIENT FLOW ---------- //
@@ -241,7 +330,7 @@ const options_manager = [
         message: 'Seleccione una opción:',
         choices: [
             {name: 'Lista de slices', value: 1},
-            {name: 'Crear slices', value: 2},            
+            {name: 'Crear slice', value: 2},            
             {name: 'Gestionar slices', value: 3},
             {name: 'Monitoreo', value: 4},
             {name: 'Configuración de templates', value: 5},
@@ -262,6 +351,18 @@ const show_slices = [
             {name: 'slice 2', value: 2},
             {name: 'slice 3', value: 3},
             {name: 'regresar', value: 4},
+        ]
+    }
+]
+const show_create_options_mng = [
+    {
+        type: 'rawlist',
+        name: 'create_options',
+        message: 'Usted esta creando un slice:',
+        choices: [
+            {name: 'Ver templates', value: 1},
+            {name: 'Crear desde cero', value: 2},
+            {name: 'regresar', value: 3},
         ]
     }
 ]
