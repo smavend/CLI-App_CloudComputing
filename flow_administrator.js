4619a69f0bfba0f000efae9e35488452e283bc68
noe <noe.jaraq@gmail.com>
2024-05-12 23:56:14 -0500

    a

--- flow_administrator.js
+++ flow_administrator.js
@@ -2,18 +2,51 @@ import inquirer from "inquirer";
 
 class AdministratorFlow{
 
+  #USERS = [
+    {
+      id: 1, 
+      usuario: 'Willy Huallpa', 
+      contraseña: 'willy', 
+      rol: 'regular', 
+    }, 
+
+  ]
+
     #options_admin = [
         {
             type: 'rawlist',
             name: 'res',
             message: 'Seleccione una opción:',
             choices: [
-                {name: 'Crear nuevo usuario', value: 1},
-                {name: 'Editar permisos de usuario', value: 2},
-                {name: 'Cerrar cesión', value: 3}
+                {name: 'Listar usuarios', value: 1},
+                {name: 'Crear nuevo usuario', value: 2},
+                {name: 'Editar permisos de usuario', value: 3},
+                {name: 'Cerrar cesión', value: 4}
             ]
         }
     ]
+
+    #show_users_options = [
+      {
+        type: 'list',
+        name: 'res',
+        message: 'Selecciona una opción:',
+        choices: [
+          {name: 'Ver slices de usuario', value: 1},
+          {name: 'Regresar', value: 2}
+        ]
+      }
+    ]
+
+    async show_users(){
+      console.log(`Se encontraron ${this.#USERS.length} slices`);
+      console.table(this.#USERS);
+      let answer;
+      do {
+        answer = await inquirer.prompt(this.#show_users_options);
+        console.log(answer);
+      } while (answer.res != 2)
+    }
 
     async start(){
         let answer;
@@ -21,11 +54,14 @@ class AdministratorFlow{
             answer = await inquirer.prompt(this.#options_admin);
             switch(answer.res){
                 case 1:
-                    break;
+                  await this.show_users();
+                  break;
                 case 2:
                     break;
+                case 3:
+                    break;
             }
-        } while(answer.res !== 3);
+        } while(answer.res !== 4);
         console.log('Administrator Flow started');
     }
 }
--- flow_manager.js
+++ flow_manager.js
@@ -2,6 +2,29 @@ import inquirer from "inquirer";
 
 class ManagerFlow{
 
+    #SLICES = [
+      {
+        id: 1,
+        nombre: "Slice1",
+        usuario_id: "1",
+        topologia: "lineal",
+        estado: "Activo",
+        numerodevms: "3",
+        fechadecreacion: "2024-05-12T12:30:13",
+        zonadisponibilidad: "Zona 2",
+      },
+      {
+        id: 2,
+        nombre: "Slice2",
+        usuario_id: "1",
+        topologia: "malla",
+        estado: "Desactivo",
+        numerodevms: "4",
+        fechadecreacion: "2024-05-12T23:40:13",
+        zonadisponibilidad: "Zona 1",
+      },
+    ]
+
     #options_manager = [
         {
             type: 'rawlist',
@@ -77,25 +100,21 @@ class ManagerFlow{
         }
     ]
 
+    #show_slices_options = [
+      {
+        type: 'list',
+        name: 'res',
+        message: 'Selecciona una opción:',
+        choices: [
+          {name: 'Ver detalles de slice', value: 1},
+          {name: 'Regresar', value: 2}
+        ]
+      }
+    ]
+
     async show_home_manager(username){
         // fetch slices data from orchestrator server
         console.log('-----\n' + username + ' > Home \n-----');
-    }
-
-    async show_slices_list_manag(user, optionName){
-        // 
-        this.show_home_level1(user.username,optionName)
-        const answers = await inquirer.prompt(show_slices);
-        switch(answers.show_slices){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
     }
 
     async show_create_manag(user, optionName){
@@ -127,16 +146,26 @@ class ManagerFlow{
       console.log('-----\n' + username + ' > Home > '+ optionName + '\n-----');
     }
 
+    async show_slices(){
+      console.log(`Se encontraron ${this.#SLICES.length} slices`);
+      console.table(this.#SLICES);
+      let answer;
+      do {
+        answer = await inquirer.prompt(this.#show_slices_options);
+        console.log(answer);
+      } while (answer.res != 2)
+    }
+
     async start(user){
         let answer;
         do {
-            await this.show_home_manager(user.username);
+            // await this.show_home_manager(user.username);
             answer = await inquirer.prompt(this.#options_manager);
             let selectedOptionName = this.#options_manager[0].choices.find(choice => choice.value === answer.options_admin).name;
 
             switch(answer.options_admin){
                 case 1: // show list of slices
-                    // await this.show_slices_list_manag(user,selectedOptionName);
+                    await this.show_slices();
                     break;
                 case 2: // create slice
                     await this.show_create_manag(user,selectedOptionName);
