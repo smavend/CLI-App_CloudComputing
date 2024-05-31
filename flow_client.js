298b4cc063948d31e45ba96935736250a8b58ca2
smavend <amanrique068@gmail.com>
2024-05-31 08:38:18 -0500

    update consolo.clear for client logout

--- flow_client.js
+++ flow_client.js
@@ -1,41 +1,43 @@
-import inquirer from "inquirer";
-import figlet from "figlet";
+import inquirer from "inquirer"
+import figlet from "figlet"
 
-class ClientFlow{
+class ClientFlow {
+	constructor(TOKEN) {
+		this.TOKEN = TOKEN
+	}
 
-    constructor(TOKEN){
-      this.TOKEN = TOKEN;
-    }
+	#options_client = [
+		{
+			type: "rawlist",
+			name: "res",
+			message: "Seleccione una opción: ",
+			choices: [
+				{ name: "Listar slices", value: 1 },
+				{ name: "Cambiar contraseña", value: 2 },
+				{ name: "Ayuda", value: 3 },
+				{ name: "Cerrar sesión", value: 4 },
+			],
+		},
+	]
 
-    #options_client = [
-        {
-            type: 'rawlist',
-            name: 'res',
-            message: 'Seleccione una opción: ',
-            choices: [
-                {name: 'Listar slices', value: 1},
-                {name: 'Cambiar contraseña', value: 2},
-                {name: 'Ayuda', value: 3},
-                {name: 'Cerrar sesión', value: 4}
-            ]
-        }
-    ]
-
-    async start(){
-      console.log(figlet.textSync('Client'));
-        let answer;
-        do{
-            answer = await inquirer.prompt(this.#options_client);
-            switch(answer.res){
-                case 1: // list slices
-                    break;
-                case 2: // update passwd
-                    break;
-                case 3: // help
-                    break;
-            }
-          } while(answer.res !== 4);
-    }
+	async start() {
+		console.log(figlet.textSync("Client"))
+		let answer
+		do {
+			answer = await inquirer.prompt(this.#options_client)
+			switch (answer.res) {
+				case 1: // list slices
+					break
+				case 2: // update passwd
+					break
+				case 3: // help
+					break
+				case 4: // logout
+					console.clear()
+					break
+			}
+		} while (answer.res !== 4)
+	}
 }
 
-export default ClientFlow;
+export default ClientFlow
